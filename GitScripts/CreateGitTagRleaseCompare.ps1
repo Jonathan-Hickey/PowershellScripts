310b0925788ea39ca564504ef7fcb78bd153ff72
Jonathan Hickey <HickeyJonathan02@gmail.com>
2020-07-19 13:46:20 +0100

    Removing hardcoded value for project url

---
+++ GitScripts/CreateGitTagRleaseCompare.ps1
@@ -0,0 +1,35 @@
+﻿function StopErrorsInPowerShellFromGitCommands()
+{
+
+    ##https://github.com/dahlbyk/posh-git/issues/109
+    ##https://stackoverflow.com/a/47232450
+    $env:GIT_REDIRECT_STDERR = '2>&1'
+}
+
+function GetLastReleaseVersion(){
+    return git tag | sort -descending | Select-Object -First 1
+}
+
+function CreateTag($version, $message, $commit)
+{
+    git tag -a $version -m "$message" $commit
+    git push --tags 
+}
+
+
+function GetComparedVersions($prevousRelease, $currentRelease){
+    
+    $remoteUrl = git config --get remote.origin.url;
+    $projectUrl = $remoteUrl.Replace(".git","")
+
+    return "$projectUrl/compare/$prevousRelease...$currentRelease";
+}
+
+
+function CreateRelease($version, $message, $commit)
+{
+    StopErrorsInPowerShellFromGitCommands
+    $LastReleaseVersion = GetLastReleaseVersion;
+    CreateTag $version $message $commit;
+    return GetComparedVersions $LastReleaseVersion $version
+}
